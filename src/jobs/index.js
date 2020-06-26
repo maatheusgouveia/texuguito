fb8a2fafd40cbf446fa334d6adc9ce587e229003
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-06-26 10:24:16 -0300

    ajustar cron jobs

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,11 +1,12 @@
 const cron = require("node-cron");
 const dailyReminder = require("./dailyReminder");
+const setMoodReminder = require("./setMoodReminder");
 
 // Lembrete de daily
-cron.schedule("0 11 * * MON-FRI", () => dailyReminder(), {
+cron.schedule("59 10 * * MON-FRI", () => dailyReminder(), {
 	timezone: "America/Sao_Paulo",
 });
 
-cron.schedule("20 16 * * MON-FRI", () => reminder(), {
+cron.schedule("20 16 * * MON-FRI", () => setMoodReminder(), {
 	timezone: "America/Sao_Paulo",
 });
